3d836ea0b5da74c8bb968afbda7345670c1b1229
Willian Barreto <ws.barreto@hotmail.com>
2023-12-06 19:13:46 -0300

    Configurando DI

---
+++ Teste.Topaz.App/Data/Repository/CompactDiscRepository.cs
@@ -0,0 +1,12 @@
+﻿using Teste.Topaz.App.Data.Context;
+using Teste.Topaz.App.Domain.Interface.Repository;
+using Teste.Topaz.App.Entity;
+
+namespace Teste.Topaz.App.Data.Repository;
+
+public class CompactDiscRepository : BaseRepository<CompactDiscEntity>, ICompactDiscRepository
+{
+    private readonly DataContext _dataContext;
+    public CompactDiscRepository(DataContext dataContext) : base(dataContext) =>
+        this._dataContext = dataContext;
+}
--- Teste.Topaz.App/Program.cs
+++ Teste.Topaz.App/Program.cs
@@ -1,7 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Teste.Topaz.App.Data.Context;
+using Teste.Topaz.App.Data.Repository;
+using Teste.Topaz.App.Domain.Interface.Repository;
+using Teste.Topaz.App.Domain.Interface.Service;
+using Teste.Topaz.App.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// DI
+builder.Services.AddScoped<ICompactDiscService, CompactDiscService>();
+builder.Services.AddScoped<ICompactDiscRepository, CompactDiscRepository>();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetSection("ConnectionStrings")["connectionString"]);
+});
 
 var app = builder.Build();
 
@@ -15,11 +31,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapRazorPages();
-
 app.Run();
